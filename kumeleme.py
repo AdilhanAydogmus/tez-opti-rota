86ff0483ff040fdda87833514a45d7d221ac56bc
Adilhan <125409095+AdilhanAydogmus@users.noreply.github.com>
2026-05-22 10:08:06 +0300

    Update kumeleme.py

--- kumeleme.py
+++ kumeleme.py
@@ -3,6 +3,7 @@ import joblib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+matplotlib.use("Agg")
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.cluster import KMeans
